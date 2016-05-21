ab91db1f5efbda50677b6bca92012373fd1c6984
Edvin Boiko <edvin1993@gmail.com>
2016-05-21 21:22:50 +0100

    implement character controller

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    public float health, jumpForce, speed;
+
+    void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    private bool isJumping = false;
+    private Rigidbody2D playerRigidBody;
+    private Animator playerAnimator;
+    private Player player;    
+
+    void Start ()
+    {
+        player = GetComponent<Player>();
+        playerRigidBody = GetComponent<Rigidbody2D>();
+        playerAnimator = GetComponent<Animator>();
+    }    
+	
+	void Update () {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += Vector3.right * player.speed * Time.deltaTime;
+            playerAnimator.SetBool("playerWalking", true);
+            transform.localScale = new Vector3 (1,1,1);
+        }
+        else if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            playerAnimator.SetBool("playerWalking", false);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += Vector3.left * player.speed * Time.deltaTime;
+            playerAnimator.SetBool("playerWalking", true);
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            playerAnimator.SetBool("playerWalking", false);
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow) && !isJumping)
+        {
+            playerRigidBody.velocity = Vector3.up * player.jumpForce;
+            playerAnimator.SetTrigger("jumped");
+            isJumping = true;
+        }
+	}
+
+    void OnCollisionEnter2D()
+    {
+        isJumping = false;
+    }
+}
