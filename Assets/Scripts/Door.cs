2259eef510f50501723fbe19c09114e9ec9ef877
Edvin Boiko <edvin1993@gmail.com>
2016-05-22 18:36:11 +0100

    implemented door

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door : MonoBehaviour {
+    
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Player player = col.gameObject.GetComponent<Player>();
+        if (player)
+        {
+            GetComponent<Animator>().SetBool("doorOpened", true);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        Player player = col.gameObject.GetComponent<Player>();
+        if (player)
+        {
+            GetComponent<Animator>().SetBool("doorOpened", false);
+        }
+    }
+
+}
